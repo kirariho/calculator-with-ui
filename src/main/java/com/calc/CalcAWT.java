c87519387f9ca6083e10d4d5b1a7546955261bac
kirariho <karakt234@gmail.com>
2025-08-13 16:23:30 +0700

    Calculator works correctly. Finished setTextForButton overloaded method: fixed arithmetic buttons, clear, calc, point and negative buttons.

--- src/main/java/com/calc/CalcAWT.java
+++ src/main/java/com/calc/CalcAWT.java
@@ -3,14 +3,16 @@ package com.calc;
 import java.awt.*;
 import java.awt.event.*;
 
-class CalcAWT extends WindowAdapter implements ActionListener {
+public class CalcAWT extends WindowAdapter implements ActionListener {
 
     Frame calcFrame;
     Label calcLabel;
     Button bnum_0, bnum_1, bnum_2, bnum_3, bnum_4, bnum_5, bnum_6, bnum_7, bnum_8, bnum_9;
     Button addButton, subButton, multButton, divButton, modButton, calcButton, clrButton, pointButton, negButton, backButton;
     double xd;
-    double number1, number2, check;
+    double number1, number2;
+    int check;
+    private String input, output;  // Переменные для getText - setText
 
     CalcAWT(){
         calcFrame = new Frame("Calc");
@@ -127,81 +129,106 @@ class CalcAWT extends WindowAdapter implements ActionListener {
    }
 
 //    Когда окно закрывается - ресурсы, занимаемые им, освобождаются
-        public void closingWindow(WindowEvent e){
-            calcFrame.dispose();
-        }
+    public void closingWindow(WindowEvent e){
+        calcFrame.dispose();
+    }
 
-        public void actionPerformed(ActionEvent e){
-            setTextForButton(bnum_1, e, '1');
-            setTextForButton(bnum_2, e, '2');
-            setTextForButton(bnum_3, e, '3');
-            setTextForButton(bnum_4, e, '4');
-            setTextForButton(bnum_5, e, '5');
-            setTextForButton(bnum_6, e, '6');
-            setTextForButton(bnum_7, e, '7');
-            setTextForButton(bnum_8, e, '8');
-            setTextForButton(bnum_9, e, '9');
-            setTextForButton(bnum_0, e, '0');
+    public void actionPerformed(ActionEvent e){
+        setTextForButton(bnum_1, e, '1');
+        setTextForButton(bnum_2, e, '2');
+        setTextForButton(bnum_3, e, '3');
+        setTextForButton(bnum_4, e, '4');
+        setTextForButton(bnum_5, e, '5');
+        setTextForButton(bnum_6, e, '6');
+        setTextForButton(bnum_7, e, '7');
+        setTextForButton(bnum_8, e, '8');
+        setTextForButton(bnum_9, e, '9');
+        setTextForButton(bnum_0, e, '0');
 
-            setTextForButton(pointButton, e, '.');
-            setTextForButton(negButton, e, '-');
-            
-            //Для пробела
-            setTextForButton(backButton, e, ' '); // надо отделить часть кода, которая отвечает за работу кнопок действий и арифметических операций
-
-
-        }
-
-        public void setTextForButton(Button buttonName, ActionEvent e, char charNumber){
-             String input, output;  // Переменные для getText - setText
-            if((e.getSource() == buttonName) && (buttonName != backButton) && (buttonName != addButton) && (buttonName != subButton) && (buttonName != multButton) && (buttonName != divButton) &&(buttonName != modButton) && (buttonName != calcButton) && (buttonName != clrButton)){
-                input = calcLabel.getText();
-                output = input + charNumber;
-                calcLabel.setText(output);
-            }
-            else if((e.getSource() == buttonName) && (buttonName == backButton) && (buttonName != addButton) && (buttonName != subButton) && (buttonName != multButton) && (buttonName != divButton) &&(buttonName != modButton)){
-                input = calcLabel.getText();
-                try{
-                    output = input.substring(0, input.length()-1);
-                }catch(StringIndexOutOfBoundsException f){
-                    return;
-                }
-                calcLabel.setText(output);
-            }
-            else if ((e.getSource() == buttonName) && ((buttonName == addButton) || (buttonName == subButton) || (buttonName == multButton) || (buttonName == divButton) || (buttonName == modButton))){
-                try{
-                    number1 = Double.parseDouble(calcLabel.getText());
-                }catch(NumberFormatException f){
-                    calcLabel.setText("Invalid format");
-                    return;
-                }
-                output = "";
-                calcLabel.setText(output);
-
-                String checkValue = buttonName.getName();
-                switch(checkValue){
-                    case "AddBtn":
-                        check = 1;
-                        break;
-                    case "SubBtn":
-                        check = 2;
-                        break;
-                    case "MultBtn":
-                        check = 3;
-                        break;
-                    case "DivBtn":
-                        check = 4;
-                        break;
-                    case "ModBtn":
-                        check = 5;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Uncnown button " + buttonName);
-                }
-            }
-        }
-    public static void main(String[] args) {
+        setTextForButton(pointButton, e, '.');
+        setTextForButton(negButton, e, '-');
         
+        //Кнопки арифметических действий
+        setTextForButton(backButton, e);
+        setTextForButton(addButton, e);
+        setTextForButton(subButton, e);
+        setTextForButton(multButton, e);
+        setTextForButton(divButton, e);
+        setTextForButton(modButton, e);
+        setTextForButton(calcButton, e);
+        setTextForButton(clrButton, e);
+    }
+
+    public void setTextForButton(Button buttonName, ActionEvent e, char charNumber){
+        // Рисуем кнопки с числами, разделителем и отрицательным числом
+            if((e.getSource() == buttonName) && ((buttonName == bnum_0) || (buttonName == negButton) || (buttonName == bnum_1) || (buttonName == pointButton) || (buttonName == bnum_1) || (buttonName == bnum_2) || (buttonName == bnum_3) || (buttonName == bnum_4) || (buttonName == bnum_5) || (buttonName == bnum_6) || (buttonName == bnum_7) || (buttonName == bnum_8) || (buttonName == bnum_9))){
+            input = calcLabel.getText();
+            output = input + charNumber;
+            calcLabel.setText(output);
+        }
+    }
+         
+    public void setTextForButton(Button buttonName, ActionEvent e){
+        if((e.getSource() == buttonName) && (buttonName == backButton)){
+            //Рисуем кнопку BackSpace
+            input = calcLabel.getText();    
+            try {
+                output = input.substring(0, input.length()-1);
+            } catch(StringIndexOutOfBoundsException f){
+                return;
+            }
+            calcLabel.setText(output);
+        }else if((e.getSource() == buttonName) && (buttonName == clrButton)){
+            number1 = 0;
+            number2 = 0;
+            check = 0;
+            xd = 0;
+            output = "";
+            calcLabel.setText(output);
+
+        } else if ((e.getSource() == buttonName) && ((buttonName == addButton) || (buttonName == subButton) || (buttonName == multButton) || (buttonName == divButton) || (buttonName == modButton))){
+            //Рисуем кнопки с арифметическими действиями
+            try {
+                number1 = Double.parseDouble(calcLabel.getText());
+            } catch(NumberFormatException f){
+                calcLabel.setText("Invalid format");
+                return;
+            }
+            calcLabel.setText("");
+
+            String checkValue = buttonName.getName();
+            switch(checkValue){
+                case "AddBtn" -> check = 1;
+                case "SubBtn" -> check = 2;
+                case "MultBtn" -> check = 3;
+                case "DivBtn" -> check = 4;
+                case "ModBtn" -> check = 5;
+                default -> throw new IllegalArgumentException("Uncnown button " + buttonName);
+            }            
+        }
+        // Кнопка результата
+        else if((e.getSource() == buttonName) && (buttonName == calcButton)){
+            try {
+                number2 = Double.parseDouble(calcLabel.getText());
+            } catch(Exception f){
+                calcLabel.setText("Enter nubber first ");
+                return;
+            }
+            
+            switch(check){
+                case 1 -> xd = number1 + number2;
+                case 2 -> xd = number1 - number2;
+                case 3 -> xd = number1 * number2;
+                case 4 -> xd = number1/number2;
+                case 5 ->  xd = number1 % number2;
+                default -> calcLabel.setText("No operation selected");
+            }
+                
+            calcLabel.setText(String.valueOf(xd));
+        }  
+    }
+    public static void main(String[] args) {
+        new CalcAWT();
     }
 }
 
